4aa5ca196c48a30ab41507aa0f54faf50ad35638
chidapach <chidapa.ch@chococrm.com>
2021-08-15 02:41:35 +0700

    [Add] Google Auth

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,106 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:foods_app/screen/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  get _googleSignIn => null;
+
+  get _auth => null;
+
+
+   Future<User> _googleSignUp() async {
+  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  final User user = (await _auth.signInWithCredential(credential)).user;
+  print("signed in ");
+  return user;
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover, image: AssetImage('assets/background.png'))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+
+              width: double.infinity,
+              //color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Sign in to continue'),
+                  Text(
+                    'Foody',
+                    style:
+                        TextStyle(fontSize: 50, color: Colors.white, shadows: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Colors.red.shade900,
+                        offset: Offset(3, 3),
+                      )
+                    ]),
+                  ),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Apple,
+                        text: "Sign up with Apple",
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign up with Google",
+                        onPressed: () {
+                          _googleSignUp().then(
+                            (value) => Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context)=> HomeScreen(),
+                          ),
+                          ));
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'By signin in you are agreeing to our',
+                        style: TextStyle(color: Colors.grey.shade800),
+                      ),
+                      Text(
+                        'Terms and Pricacy Policy',
+                        style: TextStyle(color: Colors.grey.shade800),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
